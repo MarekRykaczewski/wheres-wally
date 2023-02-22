d53a6ea878bcaa2f70fbbf8f05d2ba4aeaa54a67
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-22 17:23:45 +0100

    add ContextMenu component

---
+++ src/components/ContextMenu.js
@@ -0,0 +1,8 @@
+export const ContextMenu = (props) => {
+
+    return (
+        <div style={{top: `${props.y}px`, left: `${props.x}px`}} id="context-menu">
+            <div className="context-menu--item">Wally</div>
+        </div>
+    )
+}
--- src/components/GameWindow.js
+++ src/components/GameWindow.js
@@ -1,7 +1,34 @@
 import level1 from "../img/level_1.jpg"
+import { ContextMenu } from "./ContextMenu"
+import { useState } from "react"
+
 
 export const GameWindow = (props) => {
+
+    const initialContextMenu = {
+        show: false,
+        x: 0,
+        y: 0,
+    }
+
+    const [contextMenu, setContextMenu ] = useState(initialContextMenu)
+
+    const handleContextMenu = event => {
+        event.preventDefault()
+        console.log("clicked")
+        const { pageX, pageY } = event
+        setContextMenu({ show: true, x: pageX, y: pageY })
+    }
+
     return (
-        <img className="game-window" alt="game" src={level1} />
+        <div>
+            <img onContextMenu={handleContextMenu} id="game-window" alt="game" src={level1} />
+            {contextMenu.show && 
+            <ContextMenu
+            x={contextMenu.x}
+            y={contextMenu.y}
+            />}
+        </div>
+
     )
 }
