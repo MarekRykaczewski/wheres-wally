769a34cb8423f3baa1839587db5e9e574ae7c734
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-23 16:35:07 +0100

    add firestore to firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFireStore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyATDnym2GdGUgJZz6aqe5yia1T1kgNKh2A",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFireStore(app)
