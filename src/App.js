36695cbbe99be880f984347ec0e208c167012557
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-23 18:57:43 +0100

    handle getServerLevelsData with useEffect

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { Home } from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './config/firebase';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const getServerLevelsData = async () => {
-    const levelsServerRef = collection(db, "levels")
-    const serverLevelsData = await getDocs(levelsServerRef)
-    const filteredData = serverLevelsData.docs.map((doc) => ({...doc.data(), id: doc.id}))
-    return(filteredData)
-  }
+  const [serverLevelsData, setServerLevelsData] = useState([])
+
+  useEffect(() => {
+    const getServerLevelsData = async () => {
+      const levelsServerRef = collection(db, "levels")
+      const serverLevelsData = await getDocs(levelsServerRef)
+      const filteredData = serverLevelsData.docs.map((doc) => ({...doc.data(), id: doc.id}))
+      setServerLevelsData(filteredData)
+    }
+    getServerLevelsData()
+  }, [])
 
   let clientLevelsData = [
     {id: 1, url: "level_1.jpg", characters: { name: "wally", found: false }}, 
@@ -21,7 +27,7 @@ function App() {
     {id: 3, url: "level_3.jpg"}
   ]
 
-  const serverLevelsData = getServerLevelsData()
+  console.log(serverLevelsData)
 
   return (
     <Router>
