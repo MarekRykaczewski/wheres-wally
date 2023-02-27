3419d2af9e32aea60b94167a2306cae79871f32e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-27 21:02:39 +0100

    create addEntry method inside SubmitModal component

--- src/components/SubmitModal.js
+++ src/components/SubmitModal.js
@@ -1,4 +1,13 @@
+import { addDoc, collection } from "firebase/firestore"
+import { db } from "../config/firebase"
+
 export const SubmitModal = (props) => {
+
+    const leaderboardCollectionRef = collection(db, "leaderboard")
+
+    const addEntry = async () => {
+        await addDoc(leaderboardCollectionRef, {name: "test_user", score: props.score})
+    }
 
     return (
         <div className="modal-background">
@@ -9,7 +18,7 @@ export const SubmitModal = (props) => {
                 </div>
                 <div className="body">
                     <button className="modal-button">Cancel</button>
-                    <button className="modal-button">Continue</button>
+                    <button onClick={addEntry} className="modal-button">Continue</button>
                 </div>
             </div>
 
