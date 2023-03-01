baf6dd7de390aca9e597f97b0e2ceb269ab961f7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-01 19:04:38 +0100

    add toast to submit modal and don't allow duplicate submittions

--- src/components/SubmitModal.js
+++ src/components/SubmitModal.js
@@ -1,24 +1,34 @@
 import { addDoc, collection } from "firebase/firestore"
+import { useEffect, useState } from "react";
 import { db } from "../config/firebase"
+import { Toast } from "./Toast";
 
 export const SubmitModal = (props) => {
+
+    useEffect(() => {
+        setSubmitted(false)
+    }, [])
+
+    const [submitted, setSubmitted] = useState(false)
 
     const leaderboardCollectionRef = collection(db, "leaderboard")
 
     const addEntry = async () => {
-        if (props.username) {
+        if (props.username || submitted === false) {
             await addDoc(leaderboardCollectionRef, {name: props.username, score: props.score})
+            setSubmitted(true)
         } else {
             return
         }
     }
 
     const greyedOutButton = {
-        backgroundColor: !props.username && "grey"
+        backgroundColor: (!props.username && "grey") || (submitted === false && "grey")
     }
 
     return (
         <div className="modal-background">
+            {submitted === true && <Toast mainMessage={"Success"} subMessage={"You have submitted your score!"}/>}
             <div className="modal-container">
                 <div className="title">
                     <h1>Would you like to submit your score?</h1>
