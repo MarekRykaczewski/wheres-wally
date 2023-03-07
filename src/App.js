c587f786e2ec80ef2a9b1f63b2026f82b1dc02e2
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-07 20:32:48 +0100

    update character data for all levels

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
 
   const initialClientLevelsData = [
     {id: 1, url: "level_1.jpg", characters: { wally: {found: false, url: `../wally.png` }, odlaw: {found: false, url: `../odlaw.png`}, wizard: {found: false, url: `../wizard.png`}} }, 
-    {id: 2, url: "level_2.jpg", characters: { wally: {found: false, url: `../wally.png` }} },
+    {id: 2, url: "level_2.jpg", characters: { wally: {found: false, url: `../wally.png` }, odlaw: {found: false, url: `../odlaw.png`}, wizard: {found: false, url: `../wizard.png`}, wilma: {found: false, url: `../wilma.png`}} },
     {id: 3, url: "level_3.jpg", characters: { wally: {found: false, url: `../wally.png` }} },
-    {id: 4, url: "level_4.jpg", characters: { wally: {found: false, url: `../wally.png` }} },
-    {id: 5, url: "level_5.jpg", characters: { wally: {found: false, url: `../wally.png` }} },
-    {id: 6, url: "level_6.jpg", characters: { wally: {found: false, url: `../wally.png` }} }
+    {id: 4, url: "level_4.jpg", characters: { wally: {found: false, url: `../wally.png` }, odlaw: {found: false, url: `../odlaw.png`}, wizard: {found: false, url: `../wizard.png`}, wilma: {found: false, url: `../wilma.png`}} },
+    {id: 5, url: "level_5.jpg", characters: { wally: {found: false, url: `../wally.png` }, odlaw: {found: false, url: `../odlaw.png`}, wizard: {found: false, url: `../wizard.png`}} },
+    {id: 6, url: "level_6.jpg", characters: { wally: {found: false, url: `../wally.png` }, odlaw: {found: false, url: `../odlaw.png`}, wizard: {found: false, url: `../wizard.png`}} }
   ]
 
   const [user, setUser] = useState(null)
