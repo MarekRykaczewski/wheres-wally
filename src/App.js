e317ceb02cc725b565f879ba5f700250eaa6e7ad
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-08 16:29:11 +0100

    revert to levelData inside App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,59 @@ import { useEffect, useState } from 'react';
 import { Leaderboard } from './components/Leaderboard';
 import { auth, googleProvider } from "./config/firebase"
 import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth"
-import clientData from './clientData';
 
 function App() {
 
-  const initialClientLevelsData = clientData
+  const initialClientLevelsData = [
+    {id: 1, 
+    url: "level_1.jpg", 
+    characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }, 
+        odlaw: {id: 2, found: false, url: `../odlaw.png`}, 
+        wizard: {id: 3,found: false, url: `../wizard.png`}
+      } 
+    }, 
+    {id: 2,
+       url: "level_2.jpg", 
+       characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }, 
+        odlaw: {id: 2, found: false, url: `../odlaw.png`}, 
+        wizard: {id: 3, found: false, url: `../wizard.png`}, 
+        wilma: {id: 4, found: false, url: `../wilma.png`}
+      } 
+    },
+    {id: 3, 
+      url: "level_3.jpg", 
+      characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }
+      } 
+    },
+    {id: 4, 
+      url: "level_4.jpg", 
+      characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }, 
+        odlaw: {id: 2, found: false, url: `../odlaw.png`}, 
+        wizard: {id: 3, found: false, url: `../wizard.png`}, 
+        wilma: {id: 4, found: false, url: `../wilma.png`}
+      } 
+    },
+    {id: 5, 
+      url: "level_5.jpg", 
+      characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }, 
+        odlaw: {id: 2, found: false, url: `../odlaw.png`}, 
+        wizard: {id: 3, found: false, url: `../wizard.png`}
+      } 
+    },
+    {id: 6, 
+      url: "level_6.jpg", 
+      characters: { 
+        wally: {id: 1, found: false, url: `../wally.png` }, 
+        odlaw: {id: 2, found: false, url: `../odlaw.png`}, 
+        wizard: {id: 3, found: false, url: `../wizard.png`}
+      } 
+    }
+  ]
 
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState("")
