84382f2aea62ed915e1ab269afda6141e2133dd7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-09 17:51:32 +0100

    implement character previews for each level

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,20 @@ import { Link } from "react-router-dom";
 
 export const Home = (props) => {
 
+    const getCharacterPreviews = (level) => {
+        const characterPreviews = Object.keys(level.characters).map(character => (
+            <img className="home--level-character" alt="character" src={`../${character}.png`}></img>
+        ))
+        return characterPreviews
+    }
+
     const levelItems = props.levels.map(level => (
       <div className="home--level--container" key={level.id}> 
       <Link className="home--level-preview" to={`/level/${level.id}`}>
+      <div className="home--level-characters">
       <span className="home--level-title"> Level {level.id}</span>
+        {getCharacterPreviews(level)}
+      </div>
       <img alt="level" className="home--level-preview--image" src={level.url}></img>
       </Link>
       </div>
