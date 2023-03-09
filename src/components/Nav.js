a760171cc6d44268b7a65fc8d6c5d22a0b4f630f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-09 13:51:14 +0100

    move link around div rather than just text on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ export const Nav = (props) => {
             <Link id="home-navigation-link" to={"/"}>
             <h1 className="nav--title"> Where's Wally </h1>
             </Link>
-            <div id="home-navigation">
             <Link id="home-navigation-link" to={"/leaderboard"}>
+            <div id="home-navigation">
                 Leaderboard
-            </Link>
             </div>
+            </Link>
             <div className="auth">
             {props.username && <span className="auth--username"> Welcome, {props.username} </span>}
             {props.profilePicUrl && <img className="auth--img" alt="Profile" src={props.profilePicUrl}></img>}
